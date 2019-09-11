135a5f8cbc4a608bb6b84ee6793ceca11a5833a9
RodrigoMattosoSilveira <r3850365@gmail.com>
2019-09-11 16:24:59 -0700

    master  -  Added e2e test to validate the ability to use page objects from the @anviltech/fui-po package.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,24 @@
 import { AppPage } from './app.po';
+import { WelcomePageView } from '@anviltech/fui-po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
+  let welcome: WelcomePageView;
 
   beforeEach(() => {
     page = new AppPage();
+    welcome = new WelcomePageView();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getTitleText()).toEqual('Welcome to ng-workspace-helper!');
+  });
+
+  it('should get the answer for everything', () => {
+    page.navigateTo();
+    expect(welcome.getTheAnswerToEverything()).toEqual(42);
   });
 
   afterEach(async () => {
